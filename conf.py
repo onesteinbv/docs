cafed6a5ee45ef0b67b76ccea41631a0636bf55c
LoekZebregs <loek.zebregs@gmail.com>
2024-02-21 15:12:40 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -23,7 +23,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'agogo'
+html_theme = 'bizstyle'
 html_static_path = ['_static']
 
 html_short_title = 'Onestein-Documents'
