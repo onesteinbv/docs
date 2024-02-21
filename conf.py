fefafef3d42b98ea576e86b1e1005f177b290824
LoekZebregs <loek.zebregs@gmail.com>
2024-02-21 15:57:01 +0100

    Update conf.py
    font size attempt

--- conf.py
+++ conf.py
@@ -37,3 +37,23 @@ html_show_sourcelink = False
 body_min_width = 0
 body_max_width = 'none'
 stickysidebar = True
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    The font size ('10pt', '11pt' or '12pt').
+    
+    'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
