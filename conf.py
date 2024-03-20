06d9486ec384e232dbca5a3ab056d482aaaf23c9
Arne De Geeter <arne.de.geeter0@gmail.com>
2024-03-20 17:05:33 +0100

    get

--- conf.py
+++ conf.py
@@ -2,6 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -18,7 +19,7 @@ exclude_patterns = []
 extensions = [
     "sphinx_github_changelog",
 ]
-sphinx_github_changelog_token = "TOKEN HERE"
+sphinx_github_changelog_token = os.environ.get("gitkey") or ''
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
